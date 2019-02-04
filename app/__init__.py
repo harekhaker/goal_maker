d6d3423b510c66e3e205f514c630160b5b11833d
vova <vovich2011@ukr.net>
2019-02-04 20:30:42 +0200

    better auth

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +17,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from .forms import LoginForm
 from flask import render_template, flash, redirect
 from .models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -24,21 +26,27 @@ def login():
         return redirect('/index')
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter_by(email=form.username.data).first()
+        user.get_id()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect('/login')
         login_user(user, remember=form.remember_me.data)
-        return redirect('/index')
+        id = user.get_id()
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = '/page?id=%s'%id
+        return redirect(next_page)
+
     return render_template('login.html', title='Sign In', form=form)
 
-@app.route('/')
-@app.route('/index')
+
+@app.route('/page')
 @login_required
 def index():
-    user = {'username': 'Эльдар Рязанов'}
+
     posts = []
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home',  posts=posts)
 
 @app.route('/logout')
 def logout():
